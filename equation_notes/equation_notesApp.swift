addc90a3146c2a5b8370a089757be337d2edb004
Rhoads <rhoadscwilson@gmail.com>
2025-04-10 18:44:25 +0100

    Initial Commit

---
+++ equation_notes/equation_notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  equation_notesApp.swift
+//  equation_notes
+//
+//  Created by Rhoads Wilson on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct equation_notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
